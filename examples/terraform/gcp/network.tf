319066aaaf2af1fbef9d96f420097d224c79489f
patrickhuber <patrickhuber@users.noreply.github.com>
2025-05-02 10:19:19 -0400

    clean slate. this project needs a rethink

--- examples/terraform/gcp/network.tf
+++
@@ -1,9 +0,0 @@
-resource "google_compute_network" "pavement" {
-    name = "pavement-network"
-}
-
-resource "google_compute_subnetwork" "pavement" {
-    name = "pavement-network"    
-    ip_cidr_range = "10.0.0.0/24"
-    network = "${google_compute_network.pavement.id}"
-}
--- provider/provider.go
+++
@@ -1,5 +0,0 @@
-package provider
-
-// Provider defines a pavement provider that implements IaaS specific details
-type Provider interface {
-}
